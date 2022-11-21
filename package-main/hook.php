5ef5343adf69a3d25a594a0fc080a37f3566fe0a
tienhtpd02242 <48255077+thangtienql@users.noreply.github.com>
2022-11-21 22:56:57 +0700

    update

--- package-main/hook.php
+++ package-main/hook.php
@@ -1,33 +1,37 @@
 <?php
-// add_action( 'transition_post_status', 'wpse120996_generic_post_status_transition', 10, 3 );
-// function wpse120996_generic_post_status_transition( $new_status, $old_status, $post ) { 
-//     if ( $new_status == 'publish' && $old_status == 'auto-draft' ) {
-//         echo "
-//          <script type='text/javascript'>
-//             console.log('ok');
-//             window.open('abc.com', '_blank');
-//        </script>
-//         ";
-//     }
-// }
 
-add_action( 'admin_enqueue_scripts', 'wpse50770_add_admin_scripts', 10, 1 );
-function wpse50770_add_admin_scripts( $hook ) {
+add_action( 'admin_enqueue_scripts', 'thenewest_add_admin_scripts', 10, 1 );
+function thenewest_add_admin_scripts( $hook ) {
     global $post;
-    // print_r($hook);
-    // print_r($_GET['message']);
 
-    //Only need to enque script on the post.php page
+    $terms = get_the_terms($post->ID, 'post_sub');
+
     //Optional: restirct by post type
-    if ( 'post.php' == $hook  && 'post' == $post->post_type && $_GET['message'] == 6 ) {
+    if ( 'post.php' == $hook  && 'post' == $post->post_type && !empty($terms) && $_GET['message'] == 6 ) {
         $permalink = get_the_permalink($post->ID);
-        
-        $url = 'https://www.test.com?slug='. $permalink;
-        echo "
-        <script type='text/javascript'>
-            window.open('$url', '_blank');
-        </script>
-        ";
 
+        foreach ($terms as $key => $term) {
+            if( $key == 1 ){
+                break;
+            }else{
+                $link = get_field('link', $term->taxonomy . '_' . $term->term_id );
+                $url = $link .'?slug='. $permalink;
+
+                echo "
+                    <script type='text/javascript'>
+                        window.open('$url', '_blank');
+                    </script>
+                    ";
+            }
+        }
     }
+}
+
+add_action('admin_head', 'thenewest_custom_css_admin');
+function thenewest_custom_css_admin() {
+  echo '<style>
+    #post_sub-0{
+        display: none !important;
+    }
+  </style>';
 }
